c1d76f318f23aba3ed9001eec6a8f8fc7e9ac0d4
precise4 <hurdle-igloo-gag@duck.com>
2024-09-02 15:31:49 +1200

    added img_temp and concurrent download args plus better logging

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 import os
 import re
 import ssl
-import httpx
 import certifi
 import asyncio
 import aiohttp
 import aiofiles
 from lxml import html
 from urllib.parse import urlparse, parse_qs
-from requests_toolbelt import MultipartEncoder
+from requests_toolbelt.multipart.encoder import MultipartEncoder
 import requests
 import argparse
 import urllib.parse
@@ -16,148 +15,87 @@ import urllib.parse
 def parse_url(url):
     parsed_url = urllib.parse.urlparse(url)
     query_params = urllib.parse.parse_qs(parsed_url.query)
-
-    # Extract parameters with default values
-    d1 = query_params.get('d1', [''])[1]
+    d1 = query_params.get('d1', [''])[0]
     d2 = query_params.get('d2', [''])[0]
     c = query_params.get('c', [''])[0]
-
-    # Prepare the output
-    selected_discs = [d1, d2] if d1 or d2 else []
-    c = c if c else None
-
-    return selected_discs, c
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Utility script to upload https://caps-a-holic.com/ comparison to https://slow.pics/')
-    parser.add_argument('url', type=str, help='caps-a-holic URL')
-    parser.add_argument('--height', type=int, default=0, help='Height override')
-    args = parser.parse_args()
-
-    selected_discs, c = parse_url(args.url)
-    h = args.heigh
-
-    #print(f"# Specify disc ids (integers) uses all available by default if left empty")
-    #print(f"# eg: ['9770', '9781']")
-    print(f"Selected Discs: {selected_discs}")
-
-    if c is not None:
-        #print(f"# Movie ID eg:3937")
-        print(f"Movie ID: {c}")
-
-image_temp_dir = 'img_tmp'
-concurrent_downloads = 10
-
-# ----------------------------------------------
+    selected_disks = [d1, d2] if d1 or d2 else []
+    return selected_disks, c if c else None
 
 async def gather_pooled(n, *coros):
     sem = asyncio.Semaphore(n)
     async def sem_coro(coro):
         async with sem:
             return await coro
-    return await asyncio.gather(
-        *[sem_coro(c) for c in coros],
-        return_exceptions=True)
+    return await asyncio.gather(*[sem_coro(c) for c in coros], return_exceptions=True)
 
-# ----------------------------------------------
-
-async def disc_info(c: int):
-    print('Fetching info...')
+async def disk_info(c: int, image_temp_dir: str):
     url = f'https://caps-a-holic.com/c_list.php?c={c}'
-    res = requests.get(url)
-    tree = html.fromstring(res.content)
-    main_title = tree.xpath("//div[@class='big-header']/text()")[0]
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            res_content = await response.read()
+            tree = html.fromstring(res_content)
+            main_title = tree.xpath("//div[@class='big-header']/text()")[0]
+            url_str = str(response.url)
 
-    if 'd1=' in res.url:
-        d1id = parse_qs(urlparse(res.url).query)['d1'][0]
-        d2id = parse_qs(urlparse(res.url).query)['d2'][0]
-        info_comp_title = tree.xpath("//div[@class='c-cell' and contains(@style, '400')]")
-        d1 = info_comp_title[0].xpath("./text()")
-        d2 = info_comp_title[1].xpath("./text()")
+            if 'd1=' in url_str:
+                d1id, d2id = parse_qs(urlparse(url_str).query).get('d1')[0], parse_qs(urlparse(url_str).query).get('d2')[0]
+                info_comp_title = tree.xpath("//div[@class='c-cell' and contains(@style, '400')]")
+                d1 = info_comp_title[0].xpath("./text()")
+                d2 = info_comp_title[1].xpath("./text()")
+                return {
+                    d1id: [f"{d1[0]} {d1[1]}", re.findall(r'\d+x\d+', d1[-1])[-1]],
+                    d2id: [f"{d2[0]} {d2[1]}", re.findall(r'\d+x\d+', d2[-1])[-1]]
+                }, main_title
 
-        return {
-            d1id: [f"{d1[0]} {d1[1]}", re.findall('\d+x\d+', d1[-1])[-1]],
-            d2id: [f"{d2[0]} {d2[1]}", re.findall('\d+x\d+', d2[-1])[-1]]
-        }, main_title
+            disks = tree.xpath("//div[contains(@id, 'd_')]")
+            disk_info = {disk.get('id').replace('d_', ''): disk.xpath('.//text()') for disk in disks}
+            return disk_info, main_title
 
-    discs = tree.xpath("//div[contains(@id, 'd_')]")
-    disc_info = dict()
-    for disc in discs:
-        title = disc.xpath('.//text()')
-        id = disc.get('id').replace('d_', '')
-        disc_info[id] = title
-    return disc_info, main_title
-
-# ----------------------------------------------
-
-async def resolve_images(d1: str, d2: str):
-    # only returns images for d1
+async def resolve_images(d1: str, d2: str, c: int):
     images = []
-    url = f'https://caps-a-holic.com/c.php?\
-        d1={d1}&d2={d2}&c={c}'
-    async with httpx.AsyncClient() as client:
-        res = await client.get(url)
-    tree = html.fromstring(res.content)
+    url = f'https://caps-a-holic.com/c.php?d1={d1}&d2={d2}&c={c}'
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            res_content = await response.read()
+    tree = html.fromstring(res_content)
     image_links = tree.xpath("//a[contains(@href, 'c.php?d1=')]")
     for link in image_links:
-        images.append(
-            parse_qs(
-                urlparse(link.get('href')).query)['s1'][0])
-    return (d1, images)
+        href = link.get('href')
+        if href:
+            query = urlparse(href).query
+            s1 = parse_qs(query).get('s1')
+            if s1:
+                images.append(s1[0])
+    return d1, images
 
-# ----------------------------------------------
-
-async def gather_images(disc_ids):
-    print('Fetching image IDs...')
-    images = dict()
-    tasks = []
-    for i in range(len(disc_ids)-1):
-        d1 = disc_ids[i]
-        d2 = disc_ids[i+1]
-        tasks.append(resolve_images(d1, d2))
-    tasks.append(resolve_images(d2, d1))
+async def gather_images(disk_ids, c: int):
+    tasks = [resolve_images(disk_ids[i], disk_ids[i + 1], c) for i in range(len(disk_ids) - 1)]
+    if len(disk_ids) > 1:
+        tasks.append(resolve_images(disk_ids[-1], disk_ids[0], c))
     results = await asyncio.gather(*tasks)
-    for disc, img_id_list in results:
-        images[disc] = img_id_list
-    return images
+    return dict(results)
 
-# ----------------------------------------------
-
-async def fetch_file(id, url):
+async def fetch_file(id, url, image_temp_dir: str):
     fname = f'{id}.png'
-    ssl_context = ssl.create_default_context(
-        cafile=certifi.where())
+    ssl_context = ssl.create_default_context(cafile=certifi.where())
     conn = aiohttp.TCPConnector(ssl=ssl_context)
-    async with aiohttp.ClientSession(
-        connector=conn
-    ) as session:
+    async with aiohttp.ClientSession(connector=conn) as session:
         async with session.get(url) as resp:
-            assert resp.status == 200
+            if resp.status != 200:
+                raise Exception(f"Failed to fetch {url}")
             data = await resp.read()
-
-    async with aiofiles.open(
-        os.path.join(image_temp_dir, fname), "wb"
-    ) as outfile:
+    async with aiofiles.open(os.path.join(image_temp_dir, fname), "wb") as outfile:
         await outfile.write(data)
 
-# ----------------------------------------------
-
-async def grab_images(images: dict, height, image_temp_dir):
-    print('Downloading images...')
-    tasks = []
+async def grab_images(images: dict, height, image_temp_dir: str, concurrent_downloads: int):
     os.makedirs(image_temp_dir, exist_ok=True)
-    for value in images.values():
-        for img_id in value:
-            img_dl = f'https://caps-a-holic.com/c_image.php?\
-                max_height={height}&s={img_id}&a=0&x=0&y=0&l=1'
-            tasks.append(fetch_file(img_id, img_dl))
+    tasks = [
+        fetch_file(img_id, f'https://caps-a-holic.com/c_image.php?max_height={height}&s={img_id}&a=0&x=0&y=0&l=1', image_temp_dir)
+        for img_ids in images.values() for img_id in img_ids
+    ]
     await gather_pooled(concurrent_downloads, *tasks)
 
-# ----------------------------------------------
-
-async def slowpics_comparison(
-    comp_title, disc_info, image_data, img_dir='img_tmp'):
-    print('Uploading...')
+async def slowpics_comparison(comp_title, disk_info, image_data, img_dir: str):
     post_data = {
         'collectionName': (None, comp_title),
         'hentai': (None, 'false'),
@@ -168,56 +106,56 @@ async def slowpics_comparison(
     open_files = []
     for i, item in enumerate(z):
         for j, imgid in enumerate(item):
-            disc = disc_info[list(image_data.keys())[j]][0]
-            post_data[f'comparisons[{i}].images[{j}].name'] = (
-                None, f"{disc} | {imgid}"
-            )
-            f = open(
-                os.path.join(img_dir, f"{imgid}.png"), 'rb')
+            disk = disk_info[list(image_data.keys())[j]][0]
+            post_data[f'comparisons[{i}].images[{j}].name'] = (None, f"{disk} | {imgid}")
+            f = open(os.path.join(img_dir, f"{imgid}.png"), 'rb')
             open_files.append(f)
-            post_data[f'comparisons[{i}].images[{j}].file'] = (
-                f"{imgid}.png", f, 'image/png'
-            )
+            post_data[f'comparisons[{i}].images[{j}].file'] = (f"{imgid}.png", f, 'image/png')
 
     with requests.Session() as client:
         client.get("https://slow.pics/api/comparison")
         files = MultipartEncoder(post_data)
-        length = str(files.len)
         headers = {
-            "Content-Length": length,
+            "Content-Length": str(files.len),
             "Content-Type": files.content_type,
-            "X-XSRF-TOKEN": client.cookies.get_dict()["XSRF-TOKEN"]
+            "X-XSRF-TOKEN": client.cookies.get("XSRF-TOKEN")
         }
-        response = client.post(
-            "https://slow.pics/api/comparison",
-            data=files, headers=headers)
+        response = client.post("https://slow.pics/api/comparison", data=files, headers=headers)
         print(f'https://slow.pics/c/{response.text}')
 
     for f in open_files:
         f.close()
 
-# ----------------------------------------------
-
-async def start_process():
-    global selected_discs, image_temp_dir
+async def start_process(selected_disks, c, height, image_temp_dir, concurrent_downloads):
     image_temp_dir = os.path.abspath(image_temp_dir)
-    info, main_title = await disc_info(c)
-    height = 0
-    disc_data = []
-    if selected_discs == []:
-        selected_discs = info.keys()
-    for d_id in selected_discs:
+    info, main_title = await disk_info(c, image_temp_dir)
+    disk_data = []
+    if not selected_disks:
+        selected_disks = info.keys()
+    for d_id in selected_disks:
         d_id = str(d_id)
-        if not d_id in info: continue
-        disc_data.append(d_id)
-        height = max(height, int(info[d_id][1].rsplit('x', maxsplit=1)[-1]))
-
-    if h: height = h
+        if d_id not in info: continue
+        disk_data.append(d_id)
+        print(info[d_id])
+        height = max(height, int(info[d_id][1].rsplit('x', 1)[-1]))
+    height = height if height else height
     print(f'Height: {height}')
-    images = await gather_images(disc_data)
-    await grab_images(images, height, image_temp_dir)
-    await slowpics_comparison(main_title, info, images)
+    images = await gather_images(disk_data, c)
+    await grab_images(images, height, image_temp_dir, concurrent_downloads)
+    await slowpics_comparison(main_title, info, images, image_temp_dir)
 
-# ----------------------------------------------
-
-asyncio.run(start_process())
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Upload caps-a-holic comparison to slow.pics.')
+    parser.add_argument('url', type=str, help='caps-a-holic URL')
+    parser.add_argument('--height', type=int, default=0, help='Height override')
+    parser.add_argument('-d', '--disks', nargs='*', type=str, help='Additional disks', default=[])
+    parser.add_argument('--image_temp_dir', type=str, default='img_tmp', help='Temporary directory for images')
+    parser.add_argument('--concurrent_downloads', type=int, default=10, help='Number of concurrent downloads')
+    args = parser.parse_args()
+    selected_disks, c = parse_url(args.url)
+    height = args.height
+    selected_disks.extend(args.disks)
+    print(f"Selected disks: {selected_disks}")
+    if c is not None:
+        print(f"Movie ID: {c}")
+    asyncio.run(start_process(selected_disks, c, height, args.image_temp_dir, args.concurrent_downloads))
